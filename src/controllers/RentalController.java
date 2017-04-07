c87312ca10a5cec367f5c6576bd0a605c46b3eee
Cao Anh Quan <aquancva@gmail.com>
2017-04-07 17:43:07 +0200

    save

---
+++ src/controllers/RentalController.java
@@ -0,0 +1,18 @@
+package controllers;
+
+/**
+ * Created by caoquan on 4/7/17.
+ */
+public class RentalController {
+    public void requestAllRental() {
+
+    }
+
+    public void requestRentalBelowRent(int rent) {
+
+    }
+
+    public void requestRenterByNumberOfRooms(int numberOfRooms) {
+
+    }
+}
